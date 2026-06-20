d2f9be9309390a79e359e0d12c304f0683e6c997
Ashutosh pandey <shivampandit7985@gmail.com>
2026-06-20 11:48:20 +0530

    sorted arrays

---
+++ cheksort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class cheksort {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        boolean isSorted=true;
+        for(int i=0;i<n;i++){
+            if(arr[i]>arr[i+1]){
+                isSorted=false;
+                break;
+            }
+        }
+        if(isSorted){
+            System.out.println("The array is sorted.");
+        } else {
+            System.out.println("The array is not sorted.");
+        }
+    }
+
+
+
+
+    
+    
+}
